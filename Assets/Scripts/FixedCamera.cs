b31c89e27e4926f0b2ecac5ce85ee1d0413460b4
PedroValerio45 <opedrovalerio@gmail.com>
2025-06-03 01:50:51 +0100

    changed trees and added another empty building
    also ladder hint

--- Assets/Scripts/FixedCamera.cs
+++ Assets/Scripts/FixedCamera.cs
@@ -9,14 +9,24 @@ public class FixedCamera : MonoBehaviour
     public GameObject freeCamera;
     public Transform mainCamera;
 
+    [SerializeField] private bool isMarket;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             marketCamera.SetActive(true);
             freeCamera.SetActive(false);
-            
-            mainCamera.rotation = Quaternion.Euler(27, 0, 0);
+
+            if (isMarket)
+            {
+                
+                mainCamera.rotation = Quaternion.Euler(27, 0, 0);
+            }
+            else
+            {
+                mainCamera.rotation = Quaternion.Euler(0, 220, 0);
+            }
         }
     }
 
